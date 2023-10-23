f01d68451e49af6cea2889008c1e1819b0f23360
Laphtain Sithole <laphtainsithole@gmail.com>
2023-10-23 12:22:08 -0700

    Task 5

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,28 +1,33 @@
 #include "main.h"
 
 /**
- * print_sign - Prints the sign of a number
- * @n: The number to be checked
- * Return: 1 for positive num, -1 for negative num or zero for anything else
+ * main - check the code.
+ *
+ * Return: Always 0.
  */
-
-int print_sign(int n)
-
+int main(void)
 {
+    int r;
 
-	if (n > 0)
-	{
-	_putchar(43);
-	return (1);
-	}
-	else if (n < 0)
-	{
-	_putchar(45);
-	return (-1);
-	}
-	else
-	{
-	_putchar(48);
-	return (0);
-	}
+    r = print_sign(98);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0xff);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(-1);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
 }
