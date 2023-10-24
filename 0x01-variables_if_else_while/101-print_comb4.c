6fe2e5a135ed6a9192945d1fccb24837bebca223
Laphtain Sithole <laphtainsithole@gmail.com>
2023-10-23 20:40:56 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,18 +7,18 @@
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 
 {
 
 	int d, p, q;
-	
+
 	for (d = '0'; d < '9'; d++)
 	{
-	
+
 	for (p = d + 1; p <= '9'; p++)
 	{
-        
+
 	for (q = p + 1; q <= '9'; q++)
 	{
 	if ((p != d) != q)
@@ -33,8 +33,7 @@ int main (void)
 	}
 	}
 	}
-        }
-        putchar('\n');
+	}
+	putchar('\n');
 	return (0);
-
 }
